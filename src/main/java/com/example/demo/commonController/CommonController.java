8ac0ed5d6b58ada87ea180fef08cdeb68f5d033e
sunni75 <sunni75@naver.com>
2024-10-22 11:18:40 +0900

    test

--- src/main/java/com/example/demo/commonController/CommonController.java
+++ src/main/java/com/example/demo/commonController/CommonController.java
@@ -28,6 +28,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * test
+ */
 @Controller
 @RequestMapping("/member")
 @Slf4j
